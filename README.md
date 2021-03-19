9c59c3716526e3a9660fcd46ac685097ab88c2f5
Jim Harris <arcangyl@gmail.com>
2021-03-18 22:46:13 -0400

    Added new Google font link to head; added a dark/light theme mode; set a refresh page function to update current day time string every minute and trigger theme mode switch at specified times.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Work Day Scheduler
+
+## Description
+
+The assignment was to build a simple calendar appplication that allowed users to enter and save events for each hour slot of the work day. The time and date features were to utilize the Moment.JS libraries. In addition to the Moment.JS utilization, the application was to feature dynamically updated HTML and CSS elements. All application features have been implemented with a few additions. The theme will change after 6 PM to a dark theme mode. It will automatically revert back to light mode after midnight. The page will also auto-refresh after 1 minute to update the current date string's time.
+
+## Installation
+
+My files can be accessed in the repository [HERE](https://github.com/arcangyl1963/coding-challenge-quiz)
+The image below shows the repository where my project files are located:
+
+![Password Generator Repository](/assets/images/Quiz_repo.jpg)
+
+The web application can be viewed [HERE](https://arcangyl1963.github.io/coding-challenge-quiz/).
+
+The images below shows the various panels of the Coding Quiz Challenge Application: 
+
+The main start screen.
+
+![The main start screen.](./assets/images/Quiz_Screen_1.jpg)
+
+The first questions screen with timer start.
+
+![The first questions screen with timer start.](./assets/images/Quiz_Screen_2.jpg)
+
+The correct answers display green when selected.
+
+![The correct answers display green when selected.](./assets/images/Quiz_Screen_3.jpg)
+
+The incorrect answers display red when selected.
+
+![The incorrect answers display red when selected.](./assets/images/Quiz_Screen_4.jpg)
+
+The final question displays a Finish button the user must click to stop the timer and end the quiz.
+
+![The final question displays a Finish button the user must click to stop the timer and end the quiz.](./assets/images/Quiz_Screen_5_finish.jpg)
+
+The final screen displays the players score and a Restart button to try the quiz again.
+
+![The final screen displays the players score and a Restart button to try the quiz again.](./assets/images/Quiz_Screen_6_score.jpg)
+
+© 2021 arcangyl studios
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-// const moment = require("moment");
-
-let workDay = {
+// Create an object with keys and empty values for wdObject
+let wdObject = {
     '9 AM': '',
     '10 AM': '',
     '11 AM': '',
@@ -14,42 +13,49 @@ let workDay = {
     '7 PM': '',
     '8 PM': '',
   };
-
+  // Immediately update the shedule tasks with any new data
   $(document).ready(function() {
-      if(!localStorage.getItem('workDay')) {
-          updateCalendarTasks(workDay);
+      if(!localStorage.getItem('wdObject')) {
+          updateCalendarTasks(wdObject);
       } else {
-          updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
+          updateCalendarTasks(JSON.parse(localStorage.getItem('wdObject')));
       }
   })
+  // Set the current date string and define its displayformat
 
-  $('#currentDay').text(moment().format('dddd') + ', ' + moment().format('MMMM Do YYYY, h:mm:ss a'));
+  $('#currentDay').text(moment().format('dddd') + ', ' + moment().format('MMMM Do YYYY, h:mm a'));
 
+  // Set the text block background colors according past, current and future time states
   let count = 1;
-  for ( const property in workDay ) {
-      let eventText = '#event-text-' + count;
-      $(eventText).text(workDay[property]);
-      let timeID = '#time-' + count;
-      let currentHour = moment().hour();
-      let timeText = $(timeID).text();
-      let timeNum = getHourFromTxt(timeText);
-      if(timeNum < currentHour) {
-          $(eventText).addClass('prev-hour');
-      } else if (timeNum > currentHour) {
-          $(eventText).addClass('future-hour');
-      }else {
-          $(eventText).addClass('current-hour');
-      }
-      count++
+  for (const myItems in wdObject) {
+    let eventText = '#event-text-' + count;
+    $(eventText).text(wdObject[myItems]);
+    let timeID = '#time-' + count;
+    let currentHour = moment().hour();
+    let timeText = $(timeID).text();
+    let timeNum = getHourFromTxt(timeText);
+    if (timeNum < currentHour) {
+      $(eventText).addClass('prev-hour');
+    } else if (timeNum > currentHour) {
+      $(eventText).addClass('future-hour');
+    } else {
+      $(eventText).addClass('current-hour');
+    }
+    if (currentHour >= 18) {
+      $("body").addClass("dark-mode");
+    } else {
+      $("body").addClass("light-mode");
+    }
+    count++;
   }
-
+  // Load the text entered by a user and the corresponding hour text into variables for the save function
   $('button').click(function() {
       theVal = $(this).siblings('textarea').val();
       hrText = $(this).siblings('div').text();
 
-      saveSchedule(hrText, theVal);
+      saveTheSchedule(hrText, theVal);
   });
-
+  // Convert the hour text into an integer
   function getHourFromTxt(timeText) {
       switch(timeText) {
           case '9 AM': return 9;
@@ -66,33 +72,42 @@ let workDay = {
           case '8 PM': return 20; 
       }
   }
-
-  function loadDataset() {
-      result = localStorage.getItem('workDay')
-      return (result ? result : workDay);
+  // Retrieve the object data from local storage
+  function retrieveData() {
+      result = localStorage.getItem('wdObject')
+      return (result ? result : wdObject);
   }
-
-  function stringifyworkDay() {
-      localStorage.setItem('workDay', JSON.stringify(workDay));
+  // Save entered data to local storage as text
+  function stringifywdObject() {
+      localStorage.setItem('wdObject', JSON.stringify(wdObject));
   };
 
   function savetoLS(dayObject) {
-      localStorage.setItem('workDay', JSON.stringify(dayObject));
+      localStorage.setItem('wdObject', JSON.stringify(dayObject));
   }
 
-  function saveSchedule(hrText, val) {
-      if(!localStorage.getItem('workDay')) {
-          stringifyworkDay();
+  function saveTheSchedule(hrText, val) {
+      if(!localStorage.getItem('wdObject')) {
+          stringifywdObject();
       }
 
-      let workHours = JSON.parse(localStorage.getItem('workDay'));
+      let workHours = JSON.parse(localStorage.getItem('wdObject'));
       workHours[hrText] = val
       savetoLS(workHours);
   }
-
+// Update the calendar elements with saved task data from local storage 
   function updateCalendarTasks(dayObject) {
       $('.calendar-row').each(function(index) {
          let calRow = $(this).children('div');
          $(this).children('textarea').text(dayObject[calRow.text()]);
       })
+
   }
+// Refresh the page every 1 minute to update currentDay time and invoke the dark / light mode switch
+$(document).ready(function() {
+    setInterval('refreshPage()', 60000);
+});
+
+function refreshPage() { 
+    location.reload(); 
+}  
